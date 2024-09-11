01b8eb0b7122c132e02ea3a113d68169f3a04e37
BoschiAlbano <iprogramacion17@gmail.com>
2024-09-11 15:35:36 -0300

    css

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./home.css";
 
 const Home = () => {
     return (
         <div>
             <h1>Este es el componente Home.jsx</h1>
             <button
+            id="button"
                 onClick={() => {
                     alert("mostras alerta");
                 }}
